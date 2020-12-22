9154aa9b76ee85822ac6509077375e3c12f8b2bc
Iron-monkey <junqiao_song@163.com>
2020-12-22 23:42:12 +0800

    122

--- pages/resultShow/resultShow.js
+++ pages/resultShow/resultShow.js
@@ -5,7 +5,10 @@ Page({
    * 页面的初始数据
    */
   data: {
-    img_preview_src: ""
+    img_preview_src: "",
+    // 批改显示内容的数据
+    formula: "",
+    result: ""
   },
 
   onLoad: function () {
@@ -36,6 +39,36 @@ Page({
           key: 'photo_src',
         });
         that.getImg();
+      }
+    })
+  },
+  // 批改作业按钮
+  getIt: function () {
+    const that = this;
+    //将图片上传到服务器
+    wx.uploadFile({
+      filePath: that.data.img_preview_src,
+      name: 'imgSrc',
+      url: 'http://127.0.0.1:8000/upload',
+      success: function(res) {
+        console.log("图片上传成功" + res);
+      },
+      fail: function(res) {
+        console.log("图片上传失败" + res);
+      }
+    });
+    // 重新请求获取数据
+    wx.request({
+      url: 'http://127.0.0.1:8000/',
+      success(result) {
+        const info = result.data;
+        that.setData({
+          formula: info,
+          result: info
+        });
+      },
+      fail: function(fal) {
+        console.log(fal);
       }
     })
   }
